d9e4264a795fa7adc6e23461d2e44d2955966174
WBelucky <shintaro.kusabiraki@gmail.com>
2020-02-12 02:10:27 +0900

    add form

---
+++ frontend/pages/setting.tsx
@@ -0,0 +1,11 @@
+import Layout from '../components/Layout'
+import Button from '@material-ui/core/Button';
+
+const Index = () => (
+    <Layout>
+        <h1>会員情報設定</h1>
+        ここにすばらしい会員登録フォームができます!
+    </Layout>
+);
+
+export default Index;
