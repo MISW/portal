0dfb16149dc3c8f174844b04d1edfe1a8cbf6a05
WBelucky <shintaro.kusabiraki@gmail.com>
2020-02-12 01:37:38 +0900

    initialize frontend

---
+++ frontend/pages/links/index.tsx
@@ -0,0 +1,20 @@
+import Layout from '../../components/Layout'
+
+const Index = () => (
+    <Layout>
+        <h1>MISW便利リンクまとめ</h1>
+        <ul>
+            <li>
+                <a title="misw.jp" href="https://misw.jp">MISW公式ホームページ misw.jp</a>
+            </li>
+            <li>
+                <a title="cloud.misw.jp" href="https://cloud.misw.jp">みすくらうど</a>
+            </li>
+            <li>
+                <a title="misw.kibe.la" href="https://misw.kibe.la">MISW Kibela</a>
+            </li>
+        </ul>
+    </Layout>
+);
+
+export default Index;
