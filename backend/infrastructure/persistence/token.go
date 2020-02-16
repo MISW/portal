3a803340f6d7c02e508db6b1d47b00c98cca360f
Tsuzu <cs3238-tsuzu@users.noreply.github.com>
2020-02-16 20:29:33 +0900

    Add token persistence, Add token schema

--- backend/infrastructure/persistence/token.go
+++ backend/infrastructure/persistence/token.go
@@ -1,1 +1,116 @@
 package persistence
+
+import (
+	"context"
+	"database/sql"
+	"time"
+
+	"github.com/MISW/Portal/backend/domain"
+	"github.com/MISW/Portal/backend/domain/repository"
+	"github.com/MISW/Portal/backend/internal/db"
+	"golang.org/x/xerrors"
+)
+
+// NewTokenPersistence - ユーザのMySQL関連の実装
+func NewTokenPersistence(db db.Ext) repository.TokenRepository {
+	return &tokenPersistence{db: db}
+}
+
+type token struct {
+	ID        int       `db:"id"`
+	UserID    int       `db:"user_id"`
+	Token     string    `db:"token"`
+	ExpiredAt time.Time `db:"expired_at"`
+
+	CreatedAt time.Time `db:"created_at"`
+	UpdatedAt time.Time `db:"updated_at"`
+}
+
+func newToken(ps *domain.Token) *token {
+	return &token{
+		UserID:     ps.UserID,
+		Authorizer: ps.Authorizer,
+		Period:     ps.Period,
+		CreatedAt:  ps.CreatedAt,
+		UpdatedAt:  ps.UpdatedAt,
+	}
+}
+
+func parseToken(ps *token) *domain.Token {
+	return &domain.Token{
+		UserID:     ps.UserID,
+		Authorizer: ps.Authorizer,
+		Period:     ps.Period,
+		CreatedAt:  ps.CreatedAt,
+		UpdatedAt:  ps.UpdatedAt,
+	}
+}
+
+type tokenPersistence struct {
+	db db.Ext
+}
+
+var _ repository.TokenRepository = &tokenPersistence{}
+
+// Add - 新しいトークンの追加
+func (tp *tokenPersistence) Add(ctx context.Context, userID int, token string, expiredAt time.Time) error {
+	_, err := tp.db.Exec(
+		`INSERT INTO tokens (
+			user_id,
+			token,
+			expired_at
+		) VALUES (
+			?,?,?
+		)`, userID, token, expiredAt)
+
+	if err != nil {
+		return xerrors.Errorf("failed to add new token: %w", err)
+	}
+
+	return nil
+}
+
+// GetByToken - トークンを検索する
+func (tp *tokenPersistence) GetByToken(ctx context.Context, token string) (*domain.Token, error) {
+	res := &token{}
+
+	err := psp.db.QueryRowx(`
+		SELECT * FROM tokens WHERE token=?
+	`, token).StructScan(res)
+
+	if err != nil {
+		if err == sql.ErrNoRows {
+			return nil, domain.ErrNoToken
+		}
+
+		return nil, xerrors.Errorf("failed to find the token: %w", err)
+	}
+
+	return parseToken(res), nil
+}
+
+// Delete - トークンを削除する
+func (tp *tokenPersistence) Delete(ctx context.Context, token string) error {
+	err := tp.db.Exec(`
+		DELETE FROM tokens WHERE token=?
+	`, token)
+
+	if err != nil {
+		return xerrors.Errorf("failed to delete the token: %w", err)
+	}
+
+	return nil
+}
+
+// DeleteAll - 特定ユーザのトークンを一生削除する
+func (tp *tokenPersistence) DeleteAll(ctx context.Context, userID string) error {
+	err := tp.db.Exec(`
+		DELETE FROM tokens WHERE user_id=?
+	`, userID)
+
+	if err != nil {
+		return xerrors.Errorf("failed to delete the tokens for user: %w", err)
+	}
+
+	return nil
+}
