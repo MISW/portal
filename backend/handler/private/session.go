8cfbeeeb388d5b985d307161aa73be3d3b3fe323
Tsuzu <cs3238-tsuzu@users.noreply.github.com>
2020-02-20 02:42:10 +0900

    Add Logout handlers

---
+++ backend/handler/private/session.go
@@ -0,0 +1,47 @@
+package private
+
+import (
+	"net/http"
+	"time"
+
+	"github.com/MISW/Portal/backend/internal/cookie"
+	"github.com/MISW/Portal/backend/internal/fronterrors"
+	"github.com/MISW/Portal/backend/usecase"
+	"github.com/labstack/echo/v4"
+)
+
+// SessionHandler - セッション周りの非公開API
+type SessionHandler interface {
+	Logout(e echo.Context) error
+}
+
+// NewSessionHandler - SessionHandlerを初期化
+func NewSessionHandler(su usecase.SessionUsecase) SessionHandler {
+	return &sessionHandler{
+		su: su,
+	}
+}
+
+var _ SessionHandler = &sessionHandler{}
+
+type sessionHandler struct {
+	su usecase.SessionUsecase
+}
+
+func (s *sessionHandler) Logout(e echo.Context) error {
+	ck, err := e.Cookie(cookie.TokenCookieKey)
+
+	if err != nil {
+		return fronterrors.RespondMessage(
+			e,
+			fronterrors.NewForbidden("unauthorized"),
+		)
+	}
+
+	err = s.su.Logout(e.Request().Context(), ck.Value)
+
+	ck.Expires = time.Now().Add(-1 * time.Hour)
+	e.SetCookie(ck)
+
+	return e.NoContent(http.StatusNoContent)
+}
---
+++ backend/internal/cookie/cookie.go
@@ -0,0 +1,6 @@
+package cookie
+
+const (
+	// TokenCookieKey - Cookieの認証用トークンのキー
+	TokenCookieKey = "misw-portal-secret"
+)
