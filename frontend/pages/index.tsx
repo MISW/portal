aee3d6f21e45359cde95151bffbd97f9c6a03f58
WBelucky <shintaro.kusabiraki@gmail.com>
2020-02-20 03:41:13 +0900

    add types around user

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -3,21 +3,25 @@ import { NextPage } from "next";
 import { DefaultLayout } from "../components/layout/DefaultLayout";
 import Link from "next/link";
 import Button from "@material-ui/core/Button";
+import SignIn from "../components/RegistrationForm"
 
 const Page: NextPage = () => (
-  <DefaultLayout>
-    <h1>ここにすばらしいポータルサイトができます</h1>
-    <Link href="/links">
-      <Button className="button" variant="contained" color="primary">
-        MISW便利リンクまとめ
-      </Button>
-    </Link>
-    <Link href="/setting">
-      <Button className="button" variant="contained" color="primary">
-        会員情報設定
-      </Button>
-    </Link>
-  </DefaultLayout>
+  <>
+    <DefaultLayout>
+      <h1>ここにすばらしいポータルサイトができます</h1>
+      <Link href="/links">
+        <Button className="button" variant="contained" color="primary">
+          MISW便利リンクまとめ
+        </Button>
+      </Link>
+      <Link href="/setting">
+        <Button className="button" variant="contained" color="primary">
+          会員情報設定
+        </Button>
+      </Link>
+    </DefaultLayout>
+  <SignIn />
+  </>
 );
 
 export default Page;
