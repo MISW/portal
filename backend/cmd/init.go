49bc64e8daf43a19f11baacf3b6b89a325a017cd
Tsuzu <cs3238-tsuzu@users.noreply.github.com>
2020-02-22 17:37:28 +0900

    Add config parser

---
+++ backend/cmd/init.go
@@ -0,0 +1,82 @@
+package main
+
+import (
+	"context"
+	"time"
+
+	"github.com/MISW/Portal/backend/config"
+	"github.com/MISW/Portal/backend/infrastructure/persistence"
+	"github.com/MISW/Portal/backend/interfaces/api/private"
+	"github.com/MISW/Portal/backend/interfaces/api/public"
+	"github.com/MISW/Portal/backend/internal/db"
+	"github.com/MISW/Portal/backend/internal/oidc"
+	"github.com/MISW/Portal/backend/usecase"
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/jmoiron/sqlx"
+	"github.com/labstack/echo/v4"
+	"go.uber.org/dig"
+	"golang.org/x/xerrors"
+)
+
+func initDig(cfg *config.Config, addr string) *dig.Container {
+	c := dig.New()
+
+	err := c.Provide(func() (oidc.Authenticator, error) {
+		ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+		defer cancel()
+
+		oidcCfg := cfg.OpenIDConnect
+
+		auth, err := oidc.NewAuthenticator(
+			ctx,
+			oidcCfg.ClientID,
+			oidcCfg.ClientSecret,
+			oidcCfg.RedirectURL,
+			oidcCfg.ProviderURL,
+			[]string{"openid", "profile", "email"},
+		)
+
+		if err != nil {
+			return nil, xerrors.Errorf("failed to initialize authenticator for OpenID Connect: %w", err)
+		}
+
+		return auth, nil
+	})
+
+	if err != nil {
+		panic(err)
+	}
+
+	err = c.Provide(func() (db.Ext, error) {
+		conn, err := sqlx.Connect("mysql", cfg.Database)
+
+		if err != nil {
+			return nil, xerrors.Errorf("failed to connect to mysql: %w", err)
+		}
+
+		return conn, nil
+	})
+
+	if err != nil {
+		panic(err)
+	}
+
+	c.Provide(persistence.NewPaymentStatusPersistence)
+	c.Provide(persistence.NewTokenPersistence)
+	c.Provide(persistence.NewUserPersistence)
+
+	c.Provide(usecase.NewSessionUsecase)
+
+	c.Provide(private.NewSessionHandler)
+	c.Provide(public.NewSessionHandler)
+
+	return c
+}
+
+func initHandler(cfg *config.Config, addr string) *echo.Echo {
+	// e := echo.New()
+
+	// e.Group("/api/public/", middleware.KeyAuth(func(key string, c echo.Context) (bool, error) {
+	// 	return key == "valid-key", nil
+	// }))
+}
---
+++ backend/config/config.go
@@ -0,0 +1,42 @@
+package config
+
+import (
+	"os"
+
+	"golang.org/x/xerrors"
+	"gopkg.in/yaml.v2"
+)
+
+// OpenIDConnect - Auth0のOpenID Connectの認証設定
+type OpenIDConnect struct {
+	ClientID     string `json:"client_id" yaml:"client_id"`
+	ClientSecret string `json:"client_secret" yaml:"client_secret"`
+	RedirectURL  string `json:"redirect_url" yaml:"redirect_url"`
+	ProviderURL  string `json:"provider_url" yaml:"provider_url"`
+}
+
+// Config - 各種設定用
+type Config struct {
+	Database string `json:"database" yaml:"database"`
+
+	OpenIDConnect OpenIDConnect `json:"oidc" yaml:"oidc"`
+}
+
+// ReadConfig - configを読み込む
+func ReadConfig(name string) (*Config, error) {
+	fp, err := os.Open(name)
+
+	if err != nil {
+		return nil, xerrors.Errorf("failed to read config: %w", err)
+	}
+
+	cfg := &Config{}
+
+	err = yaml.NewDecoder(fp).Decode(cfg)
+
+	if err != nil {
+		return nil, xerrors.Errorf("failed to parse config: %w", err)
+	}
+
+	return cfg, nil
+}
