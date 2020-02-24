73a1ae27154298f9ccab90b78b2b827477018d3d
WBelucky <shintaro.kusabiraki@gmail.com>
2020-02-24 23:08:58 +0900

    fix css bug

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,21 +1,30 @@
+// https://github.com/mui-org/material-ui/blob/master/examples/nextjs/pages/_app.js 参照
+
 import React from 'react'
-import App from 'next/app'
+import NextApp from 'next/app'
 import { AppProps } from 'next/app'
 import { ThemeProvider } from '@material-ui/styles'
 import { CssBaseline, createMuiTheme } from '@material-ui/core'
 
-function MyApp({Component, pageProps}: AppProps) {
+class App extends NextApp<AppProps> {
+  componentDidMount() {
     const jssStyles = document.querySelector('#jss-server-side')
+    console.log(jssStyles)
+    console.log(jssStyles?.parentNode)
     if (jssStyles && jssStyles.parentNode) {
       jssStyles.parentNode.removeChild(jssStyles);
     }
-    alert('hoge')
+  }
+  render() {
+    const { Component, pageProps } = this.props;
     return (
       <ThemeProvider theme={createMuiTheme({})}>
         <CssBaseline />
+
         <Component {...pageProps} />
       </ThemeProvider>
-    )
+    );
+  }
 }
 
-export default MyApp
+export default App
