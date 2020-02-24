dee66f0811d903eb26b66033f73110321a9fa57f
Tsuzu <cs3238-tsuzu@users.noreply.github.com>
2020-02-25 02:34:42 +0900

    Fix callback page

--- backend/test_files/main.go
+++ backend/test_files/main.go
@@ -37,7 +37,7 @@ const Callback = `
 				method: "POST",
 				headers: {
 					"Content-Type": "application/json",
-				}
+				},
 				body: JSON.stringify({code, state}),
 			}
 		).then(function() {
