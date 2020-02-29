b6dae97537a6b6af527c2845d302f13e28e111ea
WBelucky <shintaro.kusabiraki@gmail.com>
2020-02-29 15:23:12 +0900

    _app.tsxのAPPを関数に

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,44 +1,42 @@
 // https://github.com/mui-org/material-ui/blob/master/examples/nextjs/pages/_app.js 参照
 
 import React from 'react'
-import NextApp from 'next/app'
 import { AppProps } from 'next/app'
 import { ThemeProvider } from '@material-ui/styles'
 import { CssBaseline, createMuiTheme } from '@material-ui/core'
 import { Auth, AuthContext } from '../auth/auth'
 import { NextPageContext } from 'next'
 
-class App extends NextApp<AppProps & {auth: Auth}> {
-  static async getInitialProps({
-    Component,
-    ctx
-  }: {
-    Component: any
-    ctx: NextPageContext
-  }) {
-    const auth = {token: 'hoge'} //loadAuthFromCookie(ctx)
-    const pageProps = Component.getInitialProps
-      ? await Component.getInitialProps({...ctx, auth})
-      : {}
-    return { pageProps, auth }
-  }
-  componentDidMount() {
+const App = (props: AppProps & {auth: Auth}) => {
+  React.useEffect(() => {
     const jssStyles = document.querySelector('#jss-server-side')
     if (jssStyles && jssStyles.parentNode) {
       jssStyles.parentNode.removeChild(jssStyles);
     }
-  }
-  render() {
-    const { Component, pageProps, auth } = this.props;
-    return (
-      <ThemeProvider theme={createMuiTheme({})}>
-        <CssBaseline />
-        <AuthContext.Provider value={auth}>
-          <Component {...pageProps} />
-        </AuthContext.Provider>
-      </ThemeProvider>
-    );
-  }
+  })
+  const { Component, pageProps, auth } = props;
+  return (
+    <ThemeProvider theme={createMuiTheme({})}>
+      <CssBaseline />
+      <AuthContext.Provider value={auth}>
+        <Component {...pageProps} />
+      </AuthContext.Provider>
+    </ThemeProvider>
+  );
+}
+
+App.getInitialProps = async ({
+  Component,
+  ctx
+}: {
+  Component: any
+  ctx: NextPageContext
+}) => {
+  const auth = {token: 'hoge'} //loadAuthFromCookie(ctx)
+  const pageProps = Component.getInitialProps
+    ? await Component.getInitialProps({...ctx, auth})
+    : {}
+  return { pageProps, auth }
 }
 
 export default App
