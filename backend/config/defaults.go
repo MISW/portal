0620452248d8e1fbd291a610effa2b3290ebc9bc
Tsuzu <cs3238-tsuzu@users.noreply.github.com>
2020-02-29 20:43:36 +0900

    Update config package to use confita

--- backend/config/defaults.go
+++ backend/config/defaults.go
@@ -1,7 +1,28 @@
 package config
 
 var (
-	defaultConfig = &Config{}
+	emailVerificationSubject = "メールアドレスの認証を完了してください"
+	emailVerificationBody    = `MISWへようこそ！
+このメールは自動送信されています。
+
+下記のリンクをクリックして登録を完了してください。
+不明点などがあればサークル員にお気軽にお尋ねください！
+
+{{.VerificationLink}}
+
+経営情報学会(MISW)
+`
+
+	defaultConfig = &Config{
+		Email: Email{
+			Templates: EmailTemplatesBase{
+				EmailVerification: EmailTemplateBase{
+					Subject: emailVerificationSubject,
+					Body:    emailVerificationBody,
+				},
+			},
+		},
+	}
 )
 
 func newDefaultConfig() *Config {
