d62fd786b2503a9ac03f50e9d7a8f493f3f891b0
WBelucky <shintaro.kusabiraki@gmail.com>
2020-04-01 03:23:40 +0900

    add email verification

---
+++ frontend/pages/verify_email.tsx
@@ -0,0 +1,44 @@
+import React, { useEffect } from 'react';
+import { DefaultLayout } from '../src/components/layout/DefaultLayout';
+import Typography from '@material-ui/core/Typography';
+import { useRouter } from 'next/router';
+import { NextPage } from 'next';
+
+const Page: NextPage = () =>{
+  const router = useRouter();
+  useEffect(() => {
+    const sendEmailToken = async () => {
+      const params =  new URLSearchParams(location.search);
+      const token = params.get('token');
+      if (token === null) {
+        throw new Error('There is no status and code in query parameter');
+      }
+      const res = await fetch(`${location.protocol}//${location.host}/api/public/verify_email`, {
+        headers: {
+          'Accept': 'applicaton/json, */*',
+          'Content-type': 'application/json'
+        },
+        credentials: 'include',
+        method: 'POST',
+        body: JSON.stringify({ token })
+      });
+      const body = await res.json();
+      if (res.status >= 400) {
+        console.error(res);
+        throw new Error(`Status >= 400 message = ${body.message}`);
+      }
+      await router.push('/');
+    };
+    sendEmailToken()
+      .catch((err) => { throw err;});
+  });
+  return (
+    <DefaultLayout>
+      <Typography>verify email</Typography>
+      
+      <Typography>Loading...</Typography>
+    </DefaultLayout>
+  );
+}
+
+export default Page;
