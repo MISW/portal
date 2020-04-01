c0cde69a0f4d05e0fcbc8f41bebfe4d1c8285455
WBelucky <shintaro.kusabiraki@gmail.com>
2020-04-02 07:32:43 +0900

    add update_profile

--- frontend/src/network.ts
+++ frontend/src/network.ts
@@ -1,4 +1,4 @@
-import { User, UserForSignUp } from "./user";
+import { User, UserForSignUp as UserProfile } from "./user";
 
 const getHostAPI = () => `${location.protocol}//${location.host}/api`;
 
@@ -30,7 +30,27 @@ export const getProfile = async (): Promise<User> => {
     return Promise.reject("Error: status-code >= 400");
   }
   const body = (await res.json()) as User;
+  console.log(body);
   return body;
+};
+
+export const updateProfile = async (user: UserProfile): Promise<User> => {
+  const body = JSON.stringify(user);
+  console.log(body);
+
+  const res = await fetch(`${getHostAPI()}/private/profile`, {
+    headers: {
+      "Content-Type": "application/json",
+    },
+    method: "POST",
+    body,
+  });
+  if (res.status >= 400) {
+    console.log(res);
+    return Promise.reject("Error: status-code >= 400");
+  }
+  const resUser = (res.json() as unknown) as User;
+  return resUser;
 };
 
 export const checkLoggingIn = async (): Promise<boolean> => {
@@ -42,6 +62,7 @@ export const checkLoggingIn = async (): Promise<boolean> => {
     method: "GET",
   });
   const body = await res.json();
+  console.log(body);
   switch (body.status) {
     case "OK":
       return true;
@@ -52,7 +73,7 @@ export const checkLoggingIn = async (): Promise<boolean> => {
   }
 };
 
-export const signUp = async (user: UserForSignUp) => {
+export const signUp = async (user: UserProfile) => {
   const body = JSON.stringify(user);
   console.log(body);
 
