36af277e884ea69406d99f853345b38c4c061a5d
WBelucky <shintaro.kusabiraki@gmail.com>
2020-04-02 06:28:34 +0900

    update /setting

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -50,6 +50,7 @@ module.exports = {
             "error",
             "always"
         ],
+        "@typescript-eslint/camelcase": ["error", { "properties": "never" }],
         "@typescript-eslint/explicit-function-return-type": 0,
         "@typescript-eslint/no-explicit-any": 0,
         "react-hooks/rules-of-hooks": "error",
--- frontend/pages/setting.tsx
+++ frontend/pages/setting.tsx
@@ -1,12 +1,23 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { NextPage } from "next";
 import { DefaultLayout } from "../src/components/layout/DefaultLayout";
 import RegisterForm from "../src/components/layout/RegisterForm";
+import { UserForSignUp } from "../src/user";
+import { signUp, getProfile } from "../src/network";
 
-const Page: NextPage = () => (
-  <DefaultLayout>
-    <RegisterForm formName="会員情報設定" />
-  </DefaultLayout>
-);
+const Page: NextPage = () => {
+  const [user, setUser] = useState<UserForSignUp>();
+  useEffect(() => {
+    getProfile().then((u) => setUser(u));
+  }, []);
+  const onSubmit = (user: UserForSignUp) => {
+    signUp(user)
+      .then(() => console.log("signUp!"))
+      .catch((err) => console.error(err));
+  };
+  return (
+    <DefaultLayout>{user ? <RegisterForm formName="会員情報設定" onSubmit={onSubmit} /> : "Loading..."}</DefaultLayout>
+  );
+};
 
 export default Page;
