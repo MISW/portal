656600a047c324f6fa6147a3a7cd09e0a1b7d79a
Tsuzu <cs3238-tsuzu@users.noreply.github.com>
2020-04-05 23:19:58 +0900

    Remove reverse proxy from api server

--- backend/cmd/init.go
+++ backend/cmd/init.go
@@ -3,7 +3,6 @@ package cmd
 import (
 	"context"
 	"encoding/json"
-	"net/url"
 	"os"
 
 	"github.com/MISW/Portal/backend/config"
@@ -21,7 +20,6 @@ import (
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/jmoiron/sqlx"
 	echo "github.com/labstack/echo/v4"
-	echomw "github.com/labstack/echo/v4/middleware"
 	"github.com/labstack/gommon/log"
 	"go.uber.org/dig"
 	"golang.org/x/xerrors"
@@ -181,26 +179,6 @@ func initDig(cfg *config.Config, addr string) *dig.Container {
 	return c
 }
 
-func initReverseProxy(e *echo.Echo) {
-	addr, ok := os.LookupEnv("NEXT_SERVER")
-
-	if !ok {
-		addr = "http://localhost:3000"
-	}
-
-	url, err := url.Parse(addr)
-	if err != nil {
-		e.Logger.Fatal(err)
-	}
-	targets := []*echomw.ProxyTarget{
-		{
-			URL: url,
-		},
-	}
-
-	e.Group("/*", echomw.Proxy(echomw.NewRoundRobinBalancer(targets)))
-}
-
 func initHandler(cfg *config.Config, addr string) *echo.Echo {
 	e := echo.New()
 
@@ -254,14 +232,6 @@ func initHandler(cfg *config.Config, addr string) *echo.Echo {
 	if err != nil {
 		panic(err)
 	}
-
-	// e.GET("/", echo.HandlerFunc(func(e echo.Context) error {
-	// 	return e.HTML(http.StatusOK, files.Login)
-	// }))
-	// e.GET("/callback", echo.HandlerFunc(func(e echo.Context) error {
-	// 	return e.HTML(http.StatusOK, files.Callback)
-	// }))
-	initReverseProxy(e)
 
 	if os.Getenv("DEBUG_MODE") != "" {
 		e.Logger.SetLevel(log.DEBUG)
