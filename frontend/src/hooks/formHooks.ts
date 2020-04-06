da0c0e720573d4d3c1b7b3add9054e4f7a9ec6eb
WBelucky <shintaro.kusabiraki@gmail.com>
2020-04-07 04:16:15 +0900

    delete comments

--- frontend/src/hooks/formHooks.ts
+++ frontend/src/hooks/formHooks.ts
@@ -7,11 +7,17 @@ export const useStateWithValidate = <T>(
 ) => {
   const [value, setValue] = useState<T>(initialValue);
   const [edited, setEdited] = useState<boolean>(false);
-  const onChange = useCallback((v: T) => {
-    setEdited(true);
-    setValue(v);
-  }, [setEdited, setValue]);
-  const valid = useMemo(() => validate ? validate(value) : true, [value, validate]);
+  const onChange = useCallback(
+    (v: T) => {
+      setEdited(true);
+      setValue(v);
+    },
+    [setEdited, setValue]
+  );
+  const valid = useMemo(() => (validate ? validate(value) : true), [
+    value,
+    validate,
+  ]);
   const error = useMemo(() => !valid && edited, [valid, edited]);
   return { value, onChange, error, valid };
 };
@@ -26,13 +32,22 @@ export interface FormContentProps<T> {
   error: boolean;
 }
 
+// user情報を操作するためのフックを返す.
 const useUserHooks = (genFirstYear: number, user?: Partial<UserProfile>) => {
   return {
-    email: useStateWithValidate(user?.email ?? "", (value) => /^\S+@\S+$/.test(value)),
+    email: useStateWithValidate(user?.email ?? "", (value) =>
+      /^\S+@\S+$/.test(value)
+    ),
     generation: useStateWithValidate(user?.generation ?? genFirstYear),
-    name: useStateWithValidate(user?.name ?? "", (value) => /^\S+\s\S+$/.test(value)),
-    kana: useStateWithValidate(user?.kana ?? "", (value) => /^\S+\s\S+$/.test(value)),
-    handle: useStateWithValidate(user?.handle ?? "", (value) => /^\S+$/.test(value)),
+    name: useStateWithValidate(user?.name ?? "", (value) =>
+      /^\S+\s\S+$/.test(value)
+    ),
+    kana: useStateWithValidate(user?.kana ?? "", (value) =>
+      /^\S+\s\S+$/.test(value)
+    ),
+    handle: useStateWithValidate(user?.handle ?? "", (value) =>
+      /^\S+$/.test(value)
+    ),
     sex: useStateWithValidate(user?.sex ?? "women"),
     univName: useStateWithValidate(
       user?.university?.name ?? "早稲田大学",
@@ -43,7 +58,9 @@ const useUserHooks = (genFirstYear: number, user?: Partial<UserProfile>) => {
       (value) => /^\S+$/.test(value)
     ),
     subject: useStateWithValidate(user?.university?.subject ?? ""),
-    studentId: useStateWithValidate(user?.studentId ?? "", (value) => /^\S+$/.test(value)),
+    studentId: useStateWithValidate(user?.studentId ?? "", (value) =>
+      /^\S+$/.test(value)
+    ),
     emergencyPhoneNumber: useStateWithValidate(
       user?.emergencyPhoneNumber ?? "",
       (value) => /^(0[5-9]0[0-9]{8}|0[1-9][1-9][0-9]{7})$/.test(value)
@@ -108,120 +125,3 @@ export const useUser = (
     userHooks: h,
   };
 };
-
-// export const useValidateAfterEdited = (valid: boolean) => {
-//   const [edited, setEdited] = useState(false);
-//   const touch = () => setEdited(true);
-//   const error = !valid && edited;
-//   return { touch, error };
-// };
-
-// export const useUser = (gen1stYear: number, user?: Partial<UserProfile>) => {
-//   const [email, setEmail] = useState<string>(user?.email ?? "");
-//   const [generation, setGeneration] = useState<number>(
-//     user?.generation ?? gen1stYear
-//   );
-//   const [name, setName] = useState<string>(user?.name ?? "");
-//   const [kana, setKana] = useState<string>(user?.kana ?? "");
-//   const [handle, setHandle] = useState<string>(user?.handle ?? "");
-//   const [sex, setSex] = useState<SexType>(user?.sex ?? "women");
-//   const [univName, setUnivName] = useState<string>(
-//     user?.university?.name ?? "早稲田大学"
-//   );
-//   const [department, setDepartment] = useState<string>(
-//     user?.university?.department ?? ""
-//   );
-//   const [subject, setSubject] = useState<string>(
-//     user?.university?.subject ?? ""
-//   );
-//   const [studentId, setStudentId] = useState<string>(user?.studentId ?? "");
-//   const [emergencyPhoneNumber, setPhoneNumber] = useState<string>(
-//     user?.emergencyPhoneNumber ?? ""
-//   );
-//   const [otherCircles, setOtherCircles] = useState<string>(
-//     user?.otherCircles ?? ""
-//   );
-//   const [workshops, setWorkshops] = useState<Array<string>>(
-//     user?.workshops ?? []
-//   );
-//   const [squads, setSquads] = useState<Array<string>>(user?.squads ?? []);
-
-//   const retUser: UserProfile = {
-//     id: user?.id,
-//     email,
-//     generation,
-//     name,
-//     kana,
-//     handle,
-//     sex,
-//     studentId,
-//     emergencyPhoneNumber,
-//     otherCircles,
-//     workshops,
-//     squads,
-//     university: {
-//       name: univName,
-//       department,
-//       subject,
-//     },
-//   };
-//   return {
-//     user: retUser,
-//     setFuncs: {
-//       setEmail,
-//       setGeneration,
-//       setName,
-//       setKana,
-//       setHandle,
-//       setSex,
-//       setUnivName,
-//       setDepartment,
-//       setSubject,
-//       setStudentId,
-//       setPhoneNumber,
-//       setOtherCircles,
-//       setWorkshops,
-//       setSquads,
-//     },
-//   };
-// };
-
-// export type SetUserProfileFuncs = ReturnType<typeof useUser>["setFuncs"];
-
-// export type UserValidation = {
-//   [P in keyof Omit<UserProfile, "university">]: boolean;
-// } & {
-//   university: { [P in keyof UserProfile["university"]]: boolean };
-// };
-
-// export const useUserWithValidation = (
-//   gen1stYear: number,
-//   user?: Partial<UserProfile>
-// ): {user: UserProfile; valid: UserValidation; setFuncs: SetUserProfileFuncs} => {
-//   const { user: u, setFuncs } = useUser(gen1stYear, user);
-
-//   const valid: UserValidation = {
-//     email: /^\S+@\S+$/.test(u.email),
-//     generation: true,
-//     name: /^\S+\s\S+$/.test(u.name),
-//     kana: /^\S+\s\S+$/.test(u.kana),
-//     handle: /^\S+$/.test(u.handle),
-//     sex: true,
-//     university: {
-//       name: /^\S+$/.test(u.university.name),
-//       department: /^\S+$/.test(u.university.department),
-//       subject: true,
-//     },
-//     studentId: /^\S+$/.test(u.studentId), // TODO: ガバガバ
-//     emergencyPhoneNumber: /^(0[5-9]0[0-9]{8}|0[1-9][1-9][0-9]{7})$/.test(u.emergencyPhoneNumber),
-//     otherCircles: true,
-//     workshops: u.workshops.length !== 0,
-//     squads: true,
-//   };
-
-//   return {
-//     user: u,
-//     valid,
-//     setFuncs
-//   };
-// };
