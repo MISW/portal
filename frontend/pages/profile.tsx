68442db1e08827d7519bf395cde1efd2086820ba
WBelucky <shintaro.kusabiraki@gmail.com>
2020-04-10 02:32:09 +0900

    add katakana validation

--- frontend/pages/profile.tsx
+++ frontend/pages/profile.tsx
@@ -7,12 +7,16 @@ import { getProfile, updateProfile } from "../src/network";
 
 const Page: NextPage = () => {
   const [user, setUser] = useState<UserProfile>();
+  const [settingComplete, setSettingComplete] = useState(false);
   useEffect(() => {
     getProfile().then((u) => setUser(u));
   }, []);
   const onSubmit = (user: UserProfile) => {
     updateProfile(user)
-      .then((u) => console.log(u))
+      .then((u) => {
+        console.log(u);
+        setSettingComplete(true);
+      })
       .catch((err) => console.error(err));
   };
   return (
