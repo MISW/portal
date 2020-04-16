7883ac4a06af42c0162c33b22116bf5b5bb92031
WBelucky <shintaro.kusabiraki@gmail.com>
2020-04-17 05:40:23 +0900

    fix

--- frontend/src/components/layout/RegistrationFormStepper.tsx
+++ frontend/src/components/layout/RegistrationFormStepper.tsx
@@ -4,7 +4,6 @@ import { makeStyles } from "@material-ui/core/styles";
 import Paper from "@material-ui/core/Paper";
 import { Stepper, Step, StepLabel } from "@material-ui/core";
 import Button from "@material-ui/core/Button";
-import { SubmitResult } from "./RegisterForm";
 import { useRouter } from "next/router";
 
 export const useStyles = makeStyles((theme) => ({
