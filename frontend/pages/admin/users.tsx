1ca5abea1acff4711b57b910e5b9441f348187b3
WBelucky <shintaro.kusabiraki@gmail.com>
2020-04-22 02:59:35 +0900

    fix admin user table to be typed

--- frontend/pages/admin/users.tsx
+++ frontend/pages/admin/users.tsx
@@ -1,9 +1,25 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { NextPage } from "next";
 import { Typography } from "@material-ui/core";
 import AdminUsersTable from "../../src/components/layout/AdiminUsersTable";
+import { listUsers } from "../../src/network";
+import { UserWithPaymentJSON } from "../../src/user";
 
 const Page: NextPage = () => {
+  const [users, setUsers] = useState<Array<UserWithPaymentJSON> | null>(null);
+  useEffect(() => {
+    let unmounted = false;
+    const getListUsers = async () => {
+      const u = await listUsers();
+      if (!unmounted) {
+        setUsers(u);
+      }
+    };
+    getListUsers();
+    return () => {
+      unmounted = true;
+    };
+  }, []);
   return (
     <>
       <Typography variant="h1">ユーザー一覧</Typography>
