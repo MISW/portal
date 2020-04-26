2637c169706dde26f92a95c7f9fab3c0f4200085
Tsuzu <cs3238-tsuzu@users.noreply.github.com>
2020-04-26 18:48:59 +0900

    Add current period app config

--- backend/domain/repository/appconfig_repository.go
+++ backend/domain/repository/appconfig_repository.go
@@ -1,7 +1,15 @@
 package repository
 
 type AppConfigRepository interface {
+	// payment periodは支払う対象の期間
+
 	GetPaymentPeriod() (int, error)
 
 	SetPaymentPeriod(period int) error
+
+	// current periodは現在の期間(支払済でないとメンバーでないと見做されない期間)
+
+	GetCurrentPeriod() (int, error)
+
+	SetCurrentPeriod(period int) error
 }
