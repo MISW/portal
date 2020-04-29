dc99bf5f5725c9344a5060060428b1b90fa7c280
Tsuzu <cs3238-tsuzu@users.noreply.github.com>
2020-04-29 22:55:55 +0900

    WIP: Add config usecase

--- backend/usecase/config.go
+++ backend/usecase/config.go
@@ -1,6 +1,10 @@
 package usecase
 
-import "github.com/MISW/Portal/backend/domain/repository"
+import (
+	"github.com/MISW/Portal/backend/domain/repository"
+	"github.com/MISW/Portal/backend/internal/rest"
+	"golang.org/x/xerrors"
+)
 
 // AppConfigUsecase - 管理者用、全体
 type AppConfigUsecase interface {
@@ -30,16 +34,68 @@ func NewAppConfigUsecase(appConfigRepository repository.AppConfigRepository) App
 
 var _ AppConfigUsecase = &appConfigUsecase{}
 
+func (acu *appConfigUsecase) diffPeriod(a, b int) int {
+	year := a/100 - b/100
+
+	return year*2 + (a%100-b%100)/6
+}
+
+func (acu *appConfigUsecase) validatePeriod(period int) error {
+	year := period / 100
+	month := period % 100
+
+	if year > 2100 || year < 2019 {
+		return rest.NewBadRequest("年の値が不正です")
+	}
+
+	// これ以外を追加する場合、diffPeriodも修正する必要あり
+	if month != 4 && month != 10 {
+		return rest.NewBadRequest("4月または10月のみが許可されています")
+	}
+
+	return nil
+}
+
 func (acu *appConfigUsecase) GetPaymentPeriod() (int, error) {
-	panic("not implemented")
+	period, err := acu.appConfigRepository.GetPaymentPeriod()
+
+	if err != nil {
+		return 0, xerrors.Errorf("failed to get payment period: %w", err)
+	}
+
+	return period, nil
 }
 
 func (acu *appConfigUsecase) SetPaymentPeriod(period int) error {
-	panic("not implemented")
+	if err := acu.validatePeriod(period); err != nil {
+		return err
+	}
+
+	currentPeriod, err := acu.appConfigRepository.GetCurrentPeriod()
+
+	if err != nil {
+		return xerrors.Errorf("failed to get current period: %w", err)
+	}
+
+	if period := acu.diffPeriod(period, currentPeriod); period < 0 || period > 1 {
+		return rest.NewBadRequest("支払い期間は現在の期間と同じか一つ次のみ指定できます")
+	}
+
+	if err := acu.appConfigRepository.SetPaymentPeriod(period); err != nil {
+		return xerrors.Errorf("failed to set payment period: %w", err)
+	}
+
+	return nil
 }
 
 func (acu *appConfigUsecase) GetCurrentPeriod() (int, error) {
-	panic("not implemented")
+	period, err := acu.appConfigRepository.GetCurrentPeriod()
+
+	if err != nil {
+		return 0, xerrors.Errorf("failed to get current period: %w", err)
+	}
+
+	return period, nil
 }
 
 func (acu *appConfigUsecase) SetCurrentPeriod(period int) error {
