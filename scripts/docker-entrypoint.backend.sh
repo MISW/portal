d108b52943dfed96aa0d7cfb2081ae7f5fc69623
Tsuzu <cs3238-tsuzu@users.noreply.github.com>
2020-05-03 23:19:40 +0900

    Fix bugs

--- scripts/docker-entrypoint.backend.sh
+++ scripts/docker-entrypoint.backend.sh
@@ -2,7 +2,7 @@
 
 set -eu
 
-if [[ -z "${DATABASE_URL:-}" ]]; then
+if [ -z "${DATABASE_URL:-}" ]; then
   if [[ -n "${JAWSDB_URL}" ]]; then
     export DATABASE_URL="${JAWSDB_URL}"
   else
@@ -10,11 +10,11 @@ if [[ -z "${DATABASE_URL:-}" ]]; then
   fi
 fi
 
-if [[ -z "${OIDC_REDIRECT_URL:-}" ]]; then
+if [ -z "${OIDC_REDIRECT_URL:-}" ]; then
   export OIDC_REDIRECT_URL="https://${HEROKU_APP_NAME}.herokuapp.com/callback"
 fi
 
-if [[ -z "${BASE_URL:-}" ]]; then
+if [ -z "${BASE_URL:-}" ]; then
   export BASE_URL="https://${HEROKU_APP_NAME}.herokuapp.com/"
 fi
 
