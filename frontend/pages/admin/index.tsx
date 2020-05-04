45cfe03576585ac3c8ff9a77c51e16f437fe1cc0
Tsuzu <cs3238-tsuzu@users.noreply.github.com>
2020-05-05 01:05:16 +0900

    CSVエクスポート機能の追加

--- frontend/pages/admin/index.tsx
+++ frontend/pages/admin/index.tsx
@@ -1,17 +1,18 @@
 import React from "react";
 import NextLink from "next/link";
+import Container from "@material-ui/core/Container";
 import Button from "@material-ui/core/Button";
 import { NextPage } from "next";
 import { Typography } from "@material-ui/core";
 
 const Page: NextPage = () => {
   return (
-    <>
-      <Typography variant="h1">管理者画面</Typography>
+    <Container fixed>
+      <Typography variant="h3">管理者画面</Typography>
       <NextLink href="/admin/users">
         <Button>ユーザー管理画面へ</Button>
       </NextLink>
-    </>
+    </Container>
   );
 };
 
