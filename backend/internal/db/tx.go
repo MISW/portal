33b295e102f9cd432f0acc7e65d7421b7d50e708
Tsuzu <cs3238-tsuzu@users.noreply.github.com>
2020-05-07 10:25:17 +0900

    Fix a bug

--- backend/internal/db/tx.go
+++ backend/internal/db/tx.go
@@ -2,14 +2,19 @@ package db
 
 import (
 	"context"
+	"database/sql"
 
 	"github.com/jmoiron/sqlx"
 	"golang.org/x/xerrors"
 )
 
+type txBeginnable interface {
+	BeginTxx(ctx context.Context, opts *sql.TxOptions) (*sqlx.Tx, error)
+}
+
 // RunInTransaction - トランザクションで実行
 func RunInTransaction(ctx context.Context, db Ext, fn func(ctx context.Context, db Ext) error) (err error) {
-	s, ok := db.(*sqlx.DB)
+	s, ok := db.(txBeginnable)
 
 	if !ok {
 		return xerrors.Errorf("db is not sqlx db")
