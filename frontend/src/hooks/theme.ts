9b18b701d6d337d14321dcaacf8d941a0ea5b471
Tsuzu <cs3238-tsuzu@users.noreply.github.com>
2020-05-10 19:54:32 +0900

    Fix linter errors

--- frontend/src/hooks/theme.ts
+++ frontend/src/hooks/theme.ts
@@ -1,26 +1,35 @@
-import { useState, useCallback, useMemo, useEffect } from "react";
+import { useState, useCallback, useEffect } from "react";
 
 export const useSystemColorScheme = () => {
   const [colorScheme, setColorScheme] = useState<"dark" | "light">("dark");
 
-  const callback = useCallback((param: { matches: boolean }) => {
-    if (param.matches) {
-      setColorScheme("dark");
-    } else {
-      setColorScheme("light");
-    }
-  }, []);
+  const callback = useCallback(
+    (param: { matches: boolean }) => {
+      if (param.matches) {
+        setColorScheme("dark");
+      } else {
+        setColorScheme("light");
+      }
+    },
+    [setColorScheme]
+  );
 
   useEffect(() => {
-    const matches = window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches;
+    const matches =
+      window.matchMedia &&
+      window.matchMedia("(prefers-color-scheme: dark)").matches;
     callback({ matches });
 
-    window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', callback);
+    window
+      .matchMedia("(prefers-color-scheme: dark)")
+      .addEventListener("change", callback);
 
     return () => {
-      window.matchMedia('(prefers-color-scheme: dark)').removeEventListener('change', callback);
-    }
+      window
+        .matchMedia("(prefers-color-scheme: dark)")
+        .removeEventListener("change", callback);
+    };
   }, [callback]);
 
   return colorScheme;
-}
+};
