d53c77854b5b877d53a1c7fb6307953251838828
Tosuke <tasukeprg@gmail.com>
2020-06-30 01:22:19 +0900

    feat: add an appconfig feature

--- frontend/src/store/reducer.ts
+++ frontend/src/store/reducer.ts
@@ -1,10 +1,12 @@
 import { combineReducers } from "@reduxjs/toolkit";
 import { usersReducer } from "features/users";
 import { currentUserReducer } from "features/currentUser";
+import { appconfigReducer } from "features/appconfig";
 
 const rootReducer = combineReducers({
   users: usersReducer,
   currentUser: currentUserReducer,
+  appconfig: appconfigReducer,
 });
 
 export default rootReducer;
