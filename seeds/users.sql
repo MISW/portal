7a3ef77c3fa81318f5c005ce005cf7cf1513ab4c
Tsuzu <cs3238-tsuzu@users.noreply.github.com>
2020-10-29 01:49:56 +0900

    Fix gen for tosuke in seed, Add hebi for users' seed

--- seeds/users.sql
+++ seeds/users.sql
@@ -29,8 +29,11 @@ INSERT INTO users (
     "tsuzu@example.com", 53, "TSUZU", "ツズ", "Tsuzu", "male", "Foo大学", "Bar学部", "Hoge学科", "12345678", "0123456789", "", "プログラミング", "Web", "admin", "invited", "UAJ1882QP", NULL, 1
 ),
 (
-    "tosuke@example.com", 53, "TOSUKE", "トスケ", "tosuke", "male", "Foo大学", "Bar学部", "Hoge学科", "35782161", "0123456789", "", "プログラミング", "Web", "admin", "invited", "UJDEJGY2V", NULL, 1
+    "tosuke@example.com", 54, "TOSUKE", "トスケ", "tosuke", "male", "Foo大学", "Bar学部", "Hoge学科", "35782161", "0123456789", "", "プログラミング", "Web", "admin", "invited", "UJDEJGY2V", NULL, 1
 ),
 (
     "biraki@example.com", 53, "BIRAKI", "ビラキ", "biraki", "male", "Foo大学", "Bar学部", "Hoge学科", "35782161", "0123456789", "", "プログラミング", "Web", "admin", "invited", "UAGR0VB6X", NULL, 1
+),
+(
+    "hebi@example.com", 55, "HEBI", "ヘビ", "hebi", "male", "Foo大学", "Bar学部", "Hoge学科", "35782161", "0123456789", "", "プログラミング", "Web", "admin", "invited", "U014KLTG3CZ", NULL, 1
 );
