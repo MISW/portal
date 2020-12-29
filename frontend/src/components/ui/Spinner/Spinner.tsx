0748b0a21c3aeaeb1aa581fe0b4cad77eb5a9534
tosuke <tasukeprg@gmail.com>
2020-12-29 18:14:11 +0900

    refactor: Spinner

--- frontend/src/components/ui/Spinner/Spinner.tsx
+++ frontend/src/components/ui/Spinner/Spinner.tsx
@@ -1,10 +1,22 @@
 import React from "react";
+import clsx from "clsx";
+import styles from "./Spinner.module.css";
+import type { Colors } from "themes/colors";
+import { useThemeColor } from "themes/useThemeColor";
 
-export const Spinner: React.VFC = () => (
-  <div
-    className="animate-spin w-24 h-24 rounded-full border-solid border-8 border-blue-500"
-    style={{
-      borderTopColor: "transparent",
-    }}
-  />
-);
+type SpinnerProps = {
+  spin?: boolean;
+  tintColor?: Colors;
+};
+export const Spinner: React.VFC<SpinnerProps> = ({
+  spin = true,
+  tintColor = "blue-500",
+}) => {
+  const vars = useThemeColor(["--tint-color", tintColor]);
+  return (
+    <div
+      className={clsx(styles.spinner, spin && "animate-spin", "w-24 h-24")}
+      style={vars}
+    />
+  );
+};
