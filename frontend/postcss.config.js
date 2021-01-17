04bc577e54d17c20b6e3622263946fd452a377fe
tosuke <tasukeprg@gmail.com>
2021-01-17 15:31:26 +0900

    feat: add polyfills for `:focus-visible` pseudo class

--- frontend/postcss.config.js
+++ frontend/postcss.config.js
@@ -15,5 +15,6 @@ module.exports = {
         },
       },
     ],
+    "postcss-focus-visible",
   ],
 };
--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import "tailwindcss/tailwind.css";
 import "styles/global.css";
+import "focus-visible";
 import type { NextPage } from "next";
 import type { AppProps } from "next/app";
 import { HTTPClientProvider } from "infra/api";
