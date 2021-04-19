28fdefea5991656143e81a59725f07990f032e33
tosuke <tasukeprg@gmail.com>
2021-04-19 15:12:58 +0900

    refactor: some refactors

--- scripts/docker-entrypoint.frontend.sh
+++ scripts/docker-entrypoint.frontend.sh
@@ -3,10 +3,4 @@
 set -eu
 
 npm install
-
-if [ "${ENVIRONMENT:-}" = "prod" ]; then
-    npm run build
-    npm run start
-elif [ "${ENVIRONMENT:-}" = "dev" ]; then
-    npm run docker
-fi
+npm run docker
