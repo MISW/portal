35f5de36dede800cbd28788b4dc1ab44c97faee0
tosuke <tasukeprg@gmail.com>
2021-05-06 02:14:04 +0900

    refactor: add Button component

---
+++ frontend/src/components/design/Button/colors.ts
@@ -0,0 +1,5 @@
+export const colors = {
+  blue: "bg-blue-500 hover:bg-blue-400 active:bg-blue-600",
+  red: "bg-red-500 hover:bg-red-400 active:bg-red-600",
+  green: "bg-green-500 hover:bg-green-400 active:bg-green-600",
+};
