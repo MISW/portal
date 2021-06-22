df2335a3215fc6f7ad8702ab4d46d3018d27337b
tosuke <tasukeprg@gmail.com>
2021-06-22 21:31:22 +0900

    feat: introduce tailwind-variant.macro

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,6 +1,18 @@
+const transformer = (ext) => (content) =>
+  require("@babel/core").transformSync(content, { filename: `content.${ext}` })
+    .code;
+
 /** @type{ import('tailwindcss/tailwind-config').TailwindConfig */
 module.exports = {
   mode: "jit",
-  purge: ["src/components/**/*.{ts,tsx}", "src/pages/**/*.{ts,tsx}"],
+  purge: {
+    content: ["src/components/**/*.{ts,tsx}", "src/pages/**/*.{ts,tsx}"],
+    transform: {
+      ".ts": transformer("ts"),
+      ".tsx": transformer("tsx"),
+      ts: transformer("ts"),
+      tsx: transformer("tsx"),
+    },
+  },
   darkMode: "media",
 };
