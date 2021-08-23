b1c328e31c1b921b04ede82475e96a29c41c6ae9
tosuke <tasukeprg@gmail.com>
2021-08-23 21:24:06 +0900

    feat(backend/external-integration): add external API to dump all member roles

--- backend/usecase/externalIntegration.go
+++ backend/usecase/externalIntegration.go
@@ -1,25 +1,30 @@
 package usecase
 
 import (
+	"context"
+
 	"github.com/MISW/Portal/backend/domain"
 	"github.com/MISW/Portal/backend/domain/repository"
 	"github.com/MISW/Portal/backend/internal/rest"
 	"golang.org/x/xerrors"
 )
 
-func NewExternalIntegrationUsecase(repo repository.ExternalIntegrationRepository) ExternalIntegrationUsecase {
+func NewExternalIntegrationUsecase(repo repository.ExternalIntegrationRepository, userRepo repository.UserRepository) ExternalIntegrationUsecase {
 	return &externalIntegrationUsecase{
 		repo: repo,
+		userRepo: userRepo,
 	}
 }
 
 // ExternalIntegrationUsecase - 外部連携サービスのための
 type ExternalIntegrationUsecase interface {
 	GetUserRoleFromSlackID(slackID string) (string, error)
+	GetAllMemberRolesBySlackID(ctx context.Context) (map[string] string, error)
 }
 
 type externalIntegrationUsecase struct {
 	repo repository.ExternalIntegrationRepository
+	userRepo repository.UserRepository
 }
 
 var _ ExternalIntegrationUsecase = &externalIntegrationUsecase{}
@@ -36,4 +41,22 @@ func (u *externalIntegrationUsecase) GetUserRoleFromSlackID(slackID string) (str
 	}
 
 	return role, nil
+}
+
+func (u *externalIntegrationUsecase) GetAllMemberRolesBySlackID(ctx context.Context) (map[string] string, error) {
+	users, err := u.userRepo.List(ctx)
+
+	if err != nil {
+		return nil, xerrors.Errorf("failed to get user roles: %w", err)
+	}
+
+	roles := map[string]string{}
+	for _, user := range users {
+		if user.SlackID == "" {
+			continue
+		}
+		roles[user.SlackID] = string(user.Role)
+	}
+
+	return roles, nil
 }
