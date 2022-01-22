07a18bd305486d85a5d391e26a1616c0a9877640
Caffee <PascalCoffeeLake@gmail.com>
2022-01-23 00:58:14 +0900

    Fix "The Next.js plugin was not detected in your ESLint configuration."

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -2,6 +2,7 @@ const withTM = require("next-transpile-modules")(["ky"]);
 
 /** @type {import('next/dist/next-server/server/config-shared')} */
 const config = {
+  reactStrictMode: true,
   async rewrites() {
     return [{ source: "/card-image/:path*", destination: "/api/card/:path*" }];
   },
