78751bb2d7565ed99787bdeed3a5e9d81959e52b
CaffeeLake <PascalCoffeeLake@gmail.com>
2022-04-14 18:11:11 +0900

    Update: Packages

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,16 +1,6 @@
-const transformer = (ext) => (content) =>
-  require('@babel/core').transformSync(content, { filename: `content.${ext}` })
-    .code;
-
 module.exports = {
   content: [
     'src/pages/**/*.{js,ts,jsx,tsx}',
     'src/components/**/*.{js,ts,jsx,tsx}',
   ],
-  transform: {
-    '.ts': transformer('ts'),
-    '.tsx': transformer('tsx'),
-    ts: transformer('ts'),
-    tsx: transformer('tsx'),
-  },
 };
