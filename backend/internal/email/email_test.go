579e17d9c908690b23b8b260bcc178998e8233f1
SnakeSneakS <jealousit@icloud.com>
2022-08-09 17:32:29 +0900

    fix email test

--- backend/internal/email/email_test.go
+++ backend/internal/email/email_test.go
@@ -9,11 +9,11 @@ import (
 
 //go test -run TestSendEmail github.com/MISW/Portal/backend/internal/email -v
 func TestSendEmail(t *testing.T) {
-	s := os.Getenv("server")   //smtp server
-	u := os.Getenv("username") //smtp username
-	pass := os.Getenv("pass")  //smtp password
-	from := os.Getenv("from")  //email from
-	to := os.Getenv("to")      //email to
+	s := os.Getenv("TEST_EMAIL_SMTP_SERVER") //smtp server
+	pass := os.Getenv("TEST_EMAIL_PASS")     //smtp password
+	from := os.Getenv("TEST_EMAIL_FROM")     //email from
+	u := os.Getenv("TEST_EMAIL_TO")          //smtp username
+	to := os.Getenv("TEST_EMAIL_TO")         //email to
 	sender := email.NewSender(s, u, pass, from)
 	if err := sender.Send(to, "test subject", "test body"); err != nil {
 		t.Fatalf("failed to send email: %+v", err)
