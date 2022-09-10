1e3adf3936d534e30a4f1136dbc10aeb0b5acff2
SnakeSneakS <jealousit@icloud.com>
2022-09-10 11:49:54 +0900

    fix email test: 環境変数の値を合わせた

--- backend/internal/email/email_test.go
+++ backend/internal/email/email_test.go
@@ -9,11 +9,17 @@ import (
 
 // go test -run TestSendEmail github.com/MISW/Portal/backend/internal/email -v
 func TestSendEmail(t *testing.T) {
-	s := os.Getenv("TEST_EMAIL_SMTP_SERVER") //smtp server
-	pass := os.Getenv("TEST_EMAIL_PASS")     //smtp password
-	from := os.Getenv("TEST_EMAIL_FROM")     //email from
-	u := os.Getenv("TEST_EMAIL_USER")        //smtp username
-	to := os.Getenv("TEST_EMAIL_TO")         //email to
+	s, ok1 := os.LookupEnv("SMTP_SERVER")      //smtp server
+	pass, ok2 := os.LookupEnv("SMTP_PASSWORD") //smtp password
+	from, ok3 := os.LookupEnv("SMTP_FROM")     //email from
+	u, ok4 := os.LookupEnv("SMTP_USERNAME")    //smtp username
+	to, ok5 := os.LookupEnv("TEST_EMAIL_TO")   //email to
+
+	if !(ok1 && ok2 && ok3 && ok4 && ok5) {
+		t.Errorf("some of env values are not set")
+		return
+	}
+
 	sender := email.NewSender(s, u, pass, from)
 	if err := sender.Send(to, "test subject", "test body"); err != nil {
 		t.Errorf("failed to send email: %+v", err)
