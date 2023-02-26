20a7371db2af12a571e1105e0baad07fef72d775
CaffeeLake <PascalCoffeeLake@gmail.com>
2023-02-27 07:56:37 +0900

    Avoid: Unmaintained Packages

--- backend/config/config.go
+++ backend/config/config.go
@@ -1,84 +1,58 @@
 package config
 
 import (
-	"context"
 	"fmt"
-	"os"
-	"path/filepath"
-	"strings"
 
-	"github.com/heetch/confita"
-	"github.com/heetch/confita/backend"
-	"github.com/heetch/confita/backend/file"
+	"github.com/caarlos0/env/v7"
 	"golang.org/x/xerrors"
 )
 
 // OpenIDConnect - Auth0のOpenID Connectの認証設定
 type OpenIDConnect struct {
-	ClientID     string `config:"oidc-client-id" json:"client_id" yaml:"client_id"`
-	ClientSecret string `config:"oidc-client-secret" json:"client_secret" yaml:"client_secret"`
-	RedirectURL  string `config:"oidc-redirect-url" json:"redirect_url" yaml:"redirect_url"`
-	ProviderURL  string `config:"oidc-provider-url" json:"provider_url" yaml:"provider_url"`
+	ClientID     string `env:"OIDC_CLIENT_ID"`
+	ClientSecret string `env:"OIDC_CLIENT_SECRET"`
+	RedirectURL  string `env:"OIDC_REDIRECT_URL"`
+	ProviderURL  string `env:"OIDC_PROVIDER_URL"`
 }
 
 // Email - Email周りの設定
 type Email struct {
-	SMTPServer string `config:"smtp_server" json:"smtp_server" yaml:"smtp_server"`
-	Username   string `config:"smtp_username" json:"username" yaml:"username"`
-	Password   string `config:"smtp_password" json:"password" yaml:"password"`
-	From       string `config:"smtp_from" json:"from" yaml:"from"`
+	SMTPServer string `env:"SMTP_SERVER"`
+	Username   string `env:"SMTP_USERNAME"`
+	Password   string `env:"SMTP_PASSWORD"`
+	From       string `env:"SMTP_FROM"`
 }
 
 // Config - 各種設定用
 type Config struct {
-	Database                  string   `config:"database-url" json:"database" yaml:"database"`
-	BaseURL                   string   `config:"base-url" json:"base_url" yaml:"base_url"`
-	JWTKey                    string   `config:"jwt-key" json:"jwt_key" yaml:"jwt_key"`
-	ExternalIntegrationTokens []string `config:"external-integration-tokens" json:"external_integration_tokens" yaml:"external_integration_tokens"`
+	Database                  string   `env:"DATABASE_URL"`
+	BaseURL                   string   `env:"BASE_URL" envDefault:"http://localhost:10080/"`
+	JWTKey                    string   `env:"JWT_KEY"`
+	ExternalIntegrationTokens []string `env:"EXTERNAL_INTEGRATION_TOKENS"`
 
-	OpenIDConnect OpenIDConnect `json:"oidc" yaml:"oidc"`
-	Email         Email         `json:"email" yaml:"email"`
+	OpenIDConnect OpenIDConnect
+	Email         Email
 }
 
-// NewBackend creates a configuration loader that loads from the environment.
-// If the key is not found, this backend tries again by turning any kebabcase key to snakecase and
-// lowercase letters to uppercase.
-func NewBackend() backend.Backend {
-	return backend.Func("env", func(ctx context.Context, key string) ([]byte, error) {
-		if val := os.Getenv(key); val != "" {
-			return []byte(val), nil
-		}
-		key = strings.Replace(strings.ToUpper(key), "-", "_", -1)
-		if val := os.Getenv(key); val != "" {
-			return []byte(val), nil
-		}
-		return nil, backend.ErrNotFound
-	})
-}
-
-// ReadConfig - configを読み込む
 func ReadConfig() (*Config, error) {
-	var bs []backend.Backend = []backend.Backend{
-		NewBackend(),
-		file.NewOptionalBackend("/etc/portal/portal.yml"),
-	}
-	if home, err := os.UserHomeDir(); err == nil {
-		bs = append(bs, file.NewOptionalBackend(filepath.Join(home, ".config", "portal.yml")))
-	}
-	if wd, err := os.Getwd(); err == nil {
-		bs = append(bs, file.NewOptionalBackend(filepath.Join(wd, "portal.yml")))
-	}
+	var cfg Config
 
-	cfg := newDefaultConfig()
-	loader := confita.NewLoader(bs...)
-
-	err := loader.Load(context.Background(), cfg)
-
+	err := env.Parse(&cfg)
 	if err != nil {
-		return nil, xerrors.Errorf("failed to load config: %w", err)
+		return nil, xerrors.Errorf("failed to perse config: %w", err)
+	}
+
+	err = env.Parse(&cfg.OpenIDConnect)
+	if err != nil {
+		return nil, xerrors.Errorf("failed to perse config: %w", err)
+	}
+
+	err = env.Parse(&cfg.Email)
+	if err != nil {
+		return nil, xerrors.Errorf("failed to perse config: %w", err)
 	}
 
 	fmt.Println(cfg)
 
-	return cfg, nil
+	return &cfg, err
 }
